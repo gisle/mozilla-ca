900046f02201ce1e8de338618291c1d72bce4063
Gisle Aas <gisle@aas.no>
2011-01-22 12:40:50 +0100

    Provide interface for obtaining the path to the pem file

--- lib/Mozilla/CA.pm
+++ lib/Mozilla/CA.pm
@@ -3,6 +3,13 @@ package Mozilla::CA;
 use strict;
 our $VERSION = '20110121';
 
+use File::Basename qw(dirname);
+use File::Spec ();
+
+sub SSL_ca_file {
+    return File::Spec->catfile(dirname(__FILE__), "CA", "cacert.pem");
+}
+
 1;
 
 __END__
@@ -11,11 +18,31 @@ __END__
 
 Mozilla::CA - Mozilla's CA cert bundle in PEM format
 
+=head1 SYNOPSIS
+
+    use Mozilla::CA;
+    use IO::Socket::SSL;
+
+    my $client = IO::Socket::SSL->new(
+        PeerHost => "www.example.com:https",
+	SSL_ca_file => Mozilla::CA::SSL_ca_file(),
+    );
+
 =head1 DESCRIPTION
 
 Mozilla::CA provides a copy of Mozilla's bundle of Certificate Authority
 certificates in a form that can be consumed by modules and libraries
 based on OpenSSL.
+
+The module provide a single function:
+
+=over
+
+=item SSL_ca_file()
+
+Returns the path to the Mozilla's CA cert bundle PEM file.
+
+=back
 
 =head1 SEE ALSO
 
---
+++ t/locate-file.t
@@ -0,0 +1,18 @@
+#!perl
+
+use strict;
+use Test qw(plan ok);
+plan tests => 3;
+
+use Mozilla::CA;
+
+my $ca_file = Mozilla::CA::SSL_ca_file();
+print "# $ca_file\n";
+ok($ca_file);
+ok(open(my $fh, "<", $ca_file));
+while (<$fh>) {
+    if (/\*\* BEGIN LICENSE BLOCK \*\*/) {
+	ok(1);
+	last;
+    }
+}
