eb7efb23241cd5077c5c09d44164a6a4fadcee1d
Gisle Aas <gisle@aas.no>
2011-01-23 21:32:02 +0100

    Document license with the frontend module

--- lib/Mozilla/CA.pm
+++ lib/Mozilla/CA.pm
@@ -58,3 +58,42 @@ Returns the absolute path to the Mozilla's CA cert bundle PEM file.
 =head1 SEE ALSO
 
 L<http://curl.haxx.se/docs/caextract.html>
+
+=head1 LICENSE
+
+For the bundled Mozilla CA PEM file the following applies:
+
+=over
+
+The contents of this file are subject to the Mozilla Public License Version
+1.1 (the "License"); you may not use this file except in compliance with
+the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+for the specific language governing rights and limitations under the
+License.
+
+The Original Code is the Netscape security libraries.
+
+The Initial Developer of the Original Code is
+Netscape Communications Corporation.
+Portions created by the Initial Developer are Copyright (C) 1994-2000
+the Initial Developer. All Rights Reserved.
+
+Alternatively, the contents of this file may be used under the terms of
+either the GNU General Public License Version 2 or later (the "GPL"), or
+the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+in which case the provisions of the GPL or the LGPL are applicable instead
+of those above. If you wish to allow use of your version of this file only
+under the terms of either the GPL or the LGPL, and not to allow others to
+use your version of this file under the terms of the MPL, indicate your
+decision by deleting the provisions above and replace them with the notice
+and other provisions required by the GPL or the LGPL. If you do not delete
+the provisions above, a recipient may use your version of this file under
+the terms of any one of the MPL, the GPL or the LGPL.
+
+=back
+
+The Mozilla::CA distribution itself is available under the same license.
